dd6c3a502cc36831b7ff3b03ae4938c3208bf9bd
John Else <jelse@inmusicbrands.com>
2019-06-19 09:37:52 +0100

    Add build copying scripts

---
+++ scripts/copy-linux-builds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+DIRECTORIES="ACV5-acvs_trunk ACV8-acvs_trunk ADA2-acvs_1to1"
+
+for DIRECTORY in $DIRECTORIES
+do
+    BUILD=`ssh builds@10.95.0.141 -t "ls -t /jenkins-enos/export/$DIRECTORY | grep update | head -1 | tr -d '[:space:]'"`
+    scp builds@10.95.0.141:/jenkins-enos/export/$DIRECTORY/$BUILD .
+done
---
+++ scripts/copy-mac-builds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+WORKSPACE=/Volumes/Jenkins/Jenkins/Home/workspace
+PACKAGES_PATH=Software/Build/Mac/build/Packages
+
+DIRECTORIES="ACVS-trunk-Mac-git ACVS-1to1-Mac"
+
+for DIRECTORY in $DIRECTORIES
+do
+    mkdir -p $DIRECTORY
+    PACKAGES=$WORKSPACE/$DIRECTORY/$PACKAGES_PATH
+    BUILD=`ssh pete@10.95.0.175 -t "ls -t $PACKAGES | grep PkgFiles | head -1 | tr -d '[:space:]'"`
+    scp -r pete@10.95.0.175:$WORKSPACE/$DIRECTORY/$PACKAGES_PATH/$BUILD $DIRECTORY
+done
