79651883d1bf2b1ed509a7750139be8eaa1c3485
John Else <jelse@inmusicbrands.com>
2019-12-19 09:05:26 +0000

    Tidy copy-linux-builds.sh

--- scripts/copy-linux-builds.sh
+++ scripts/copy-linux-builds.sh
@@ -2,11 +2,13 @@
 
 set -eux
 
+BUILDS_PATH=/jenkins-enos/export
+
 DIRECTORIES="ACV5-acvs_trunk ACV8-acvs_trunk ADA2-acvs_1to1"
 
 for DIRECTORY in $DIRECTORIES
 do
     mkdir -p $DIRECTORY
-    BUILD=`ssh builds@cam-jenkins.inmusicbrands.com -t "ls -t /jenkins-enos/export/$DIRECTORY | grep update | head -1 | tr -d '[:space:]'"`
-    scp builds@cam-jenkins.inmusicbrands.com:/jenkins-enos/export/$DIRECTORY/$BUILD $DIRECTORY
+    BUILD=`ssh builds@cam-jenkins.inmusicbrands.com -t "ls -t $BUILDS_PATH/$DIRECTORY | grep update | head -1 | tr -d '[:space:]'"`
+    scp builds@cam-jenkins.inmusicbrands.com:$BUILDS_PATH/$DIRECTORY/$BUILD $DIRECTORY
 done
