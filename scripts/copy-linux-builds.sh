39affc67c028e2b91cdf1bd64d94abb1f4216d38
John Else <jelse@inmusicbrands.com>
2019-12-19 08:51:39 +0000

    Fix copy-linux-builds.sh

--- scripts/copy-linux-builds.sh
+++ scripts/copy-linux-builds.sh
@@ -2,10 +2,10 @@
 
 set -eux
 
-DIRECTORIES="ACV5-acvs_trunk ACV8-acvs_trunk ADA2-acvs_1to1 ADA2-acvs_arrange"
+DIRECTORIES="ACV5-acvs_trunk ACV8-acvs_trunk ADA2-acvs_1to1"
 
 for DIRECTORY in $DIRECTORIES
 do
-    BUILD=`ssh builds@10.95.0.141 -t "ls -t /jenkins-enos/export/$DIRECTORY | grep update | head -1 | tr -d '[:space:]'"`
-    scp builds@10.95.0.141:/jenkins-enos/export/$DIRECTORY/$BUILD .
+    BUILD=`ssh builds@cam-jenkins.inmusicbrands.com -t "ls -t /jenkins-enos/export/$DIRECTORY | grep update | head -1 | tr -d '[:space:]'"`
+    scp builds@cam-jenkins.inmusicbrands.com:/jenkins-enos/export/$DIRECTORY/$BUILD .
 done
