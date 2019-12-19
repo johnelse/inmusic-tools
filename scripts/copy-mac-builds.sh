6d24422451948f1ff5fedf945976a3de0061f80a
John Else <jelse@inmusicbrands.com>
2019-12-19 09:01:29 +0000

    Fix copy-mac-builds.sh

--- scripts/copy-mac-builds.sh
+++ scripts/copy-mac-builds.sh
@@ -2,15 +2,13 @@
 
 set -eux
 
-WORKSPACE=/Volumes/Jenkins/Jenkins/Home/workspace
-PACKAGES_PATH=Software/Build/Mac/build/Packages
+BUILDS_PATH=/Volumes/Jenkins/Builds
 
-DIRECTORIES="ACVS-trunk-Mac-git ACVS-1to1-Mac ACVS-arrange-Mac"
+DIRECTORIES="ACVS-Master-Mac ACVS-1to1-Mac"
 
 for DIRECTORY in $DIRECTORIES
 do
     mkdir -p $DIRECTORY
-    PACKAGES=$WORKSPACE/$DIRECTORY/$PACKAGES_PATH
-    BUILD=`ssh pete@10.95.0.175 -t "ls -t $PACKAGES | grep PkgFiles | head -1 | tr -d '[:space:]'"`
-    scp -r pete@10.95.0.175:$WORKSPACE/$DIRECTORY/$PACKAGES_PATH/$BUILD $DIRECTORY
+    BUILD=`ssh pete@10.95.0.175 -t "ls -t $BUILDS_PATH/$DIRECTORY | grep PkgFiles | head -1 | tr -d '[:space:]'"`
+    scp -r pete@10.95.0.175:$BUILDS_PATH/$DIRECTORY/$BUILD $DIRECTORY
 done
