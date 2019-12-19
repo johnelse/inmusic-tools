3f84e70680db9322dd85203f3a7db0fcb308a1c8
John Else <jelse@inmusicbrands.com>
2019-12-19 09:01:58 +0000

    Update copy-linux-builds.sh to put builds in subdirectories

--- scripts/copy-linux-builds.sh
+++ scripts/copy-linux-builds.sh
@@ -6,6 +6,7 @@ DIRECTORIES="ACV5-acvs_trunk ACV8-acvs_trunk ADA2-acvs_1to1"
 
 for DIRECTORY in $DIRECTORIES
 do
+    mkdir -p $DIRECTORY
     BUILD=`ssh builds@cam-jenkins.inmusicbrands.com -t "ls -t /jenkins-enos/export/$DIRECTORY | grep update | head -1 | tr -d '[:space:]'"`
-    scp builds@cam-jenkins.inmusicbrands.com:/jenkins-enos/export/$DIRECTORY/$BUILD .
+    scp builds@cam-jenkins.inmusicbrands.com:/jenkins-enos/export/$DIRECTORY/$BUILD $DIRECTORY
 done
